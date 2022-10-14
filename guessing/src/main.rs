cfd79831b70743c0364cfdbf969c436984aad1ed
Surendrakumar <p.surendrakumar@gmail.com>
2022-10-14 14:36:20 +0000

    added random number generator

--- guessing/src/main.rs
+++ guessing/src/main.rs
@@ -1,21 +1,37 @@
-use std::io;
+use std::io::{self,Write};
+use rand::Rng;
+
 fn main() {
     // user guess
     //
-    let mut input=String::new();
-    let mut guess:i32 = 0;
+    let mut rng = rand::thread_rng();
+    let mut _guess:i32 = 0;
     // random number
-    let secret_no:i32 = 0;
-    let mut choice=String::new();
+    let _secret_no:i32 = rng.gen_range(0,100);
+    let mut guess=String::new();
+    println!("secret number is {}",_secret_no);
+    
+    println!("Guess a number between 1 and 100 :999 to quit");
 
-    println!("Do you want to play:");
-    io::stdin().read_line(&mut choice).expect("error found");
-//    if (choice != 'y')
 
-    println!("Enter value:");
-    io::stdin().read(&mut input).expect("error found");
-
-    println!("{}",guess);
+    loop
+    {
+        //do not use without clearling the variable
+        guess.clear();
+        println!("Enter value:");
+        io::stdout().flush();
+        io::stdin().read_line(&mut guess).expect("error found");
+        let mut guess:i32  = guess.trim().parse().expect("Incorrect value entered by user");
+        if guess == 999 || guess == _secret_no
+        {
+            break;
+        }
+        if guess == _secret_no
+        {
+            println!("You got it !!");
+        }
+        
+    }
     // need a way to get a random number between 1 and 100
     //
     
