4bf373749de828ed75d7c7f228f69114c3e5ae6d
Surendrakumar <p.surendrakumar@gmail.com>
2022-10-14 11:15:25 +0000

    modified guess logic to read user input

--- guessing/src/main.rs
+++ guessing/src/main.rs
@@ -1,7 +1,22 @@
+use std::io;
 fn main() {
-    let guess:i32 = 0;
+    // user guess
+    //
+    let mut input=String::new();
+    let mut guess:i32 = 0;
+    // random number
     let secret_no:i32 = 0;
+    let mut choice=String::new();
+
+    println!("Do you want to play:");
+    io::stdin().read_line(&mut choice).expect("error found");
+//    if (choice != 'y')
+
+    println!("Enter value:");
+    io::stdin().read(&mut input).expect("error found");
+
+    println!("{}",guess);
     // need a way to get a random number between 1 and 100
     //
-    println!("Hello, world!");
+    
 }
