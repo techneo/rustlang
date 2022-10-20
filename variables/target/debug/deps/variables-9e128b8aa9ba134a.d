63aa75e06ef318815e0f978c5febe39cc2644ccc
Surendrakumar <p.surendrakumar@gmail.com>
2022-10-20 16:43:03 +0000

    folders

---
+++ variables/target/debug/deps/variables-9e128b8aa9ba134a.d
@@ -0,0 +1,5 @@
+/home/vagrant/code/rustlang/variables/target/debug/deps/variables-9e128b8aa9ba134a: src/main.rs
+
+/home/vagrant/code/rustlang/variables/target/debug/deps/variables-9e128b8aa9ba134a.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/vagrant/code/rustlang/variables/target/debug/variables: /home/vagrant/code/rustlang/variables/src/main.rs
