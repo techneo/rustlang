0a84adf6cd01ab626c7707156a7138b4965ec95c
Surendrakumar <p.surendrakumar@gmail.com>
2022-10-20 17:03:08 +0000

    f to c added

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    let values = [ 23,34,65,87,98,103 ];
     const PI:f64 = 3.14;
     let mut area = 0.0;
     let x:i32 =  20;
@@ -15,9 +16,17 @@ fn main() {
     };
     println!("value of {}",y);
     println!("test return {}",test(4,5));
+    for value in values {
+    println!("{value} in c is {}",temp_f_to_c(value));
+    }
 }
 
 fn test(a:u32,b:u32) -> u32
 {
     a+b
+}
+
+fn temp_f_to_c(value:u32) -> f32
+{
+    (5.0/9.0)  *(value as f32 - 32.0)
 }
