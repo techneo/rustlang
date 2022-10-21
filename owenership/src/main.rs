dc67a11dfedea463d9120a2c0d13a7fe3fcc4bc8
Surendrakumar <p.surendrakumar@gmail.com>
2022-10-21 18:02:37 +0530

    play with strings
    play with strings

--- owenership/src/main.rs
+++ owenership/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
     println!("Hello, world!");
+    {
+        { // test_var not in scope
+        let test_var = 20;   //test_var in scope
+        println!("The value is live here {test_var}");
+        } //test_var not in scope
+        // try to access the variable here
+        //println!("The value is live here {test_var}");
+
+        {
+        let mut s = String::from("hello");
+        println!("Value of string is {s}");
+
+        // not posible if s is not mutable
+        s.push_str("world");
+        println!("Value of string is {s}");
+        } // Rust calls drop once the variable is out of scope.
+
+        // will not work as the string has been deallocated from memory.
+        // s.push_str("world");
+        // println!("Value of string is {s}");
+    }
 }
